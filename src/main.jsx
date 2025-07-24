fe0c6735f5da15ab2df4b55721c75e587744f467
AmyStas <a.staszczyk@icloud.com>
2025-07-24 11:17:01 +0200

    genres page done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Genres from './Genres.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Genres />
   </StrictMode>,
 )
